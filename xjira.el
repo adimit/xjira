7134dc532ba48cc60905959dff05575a242b954f
Aleksandar Dimitrov <git@aleks.bg>
2021-08-07 16:54:57 +0200

    Prepare 0.1.0

--- xjira.el
+++ xjira.el
@@ -2,19 +2,19 @@
 ;;
 ;; Copyright (C) 2021 Aleksandar Dimitrov
 ;;
-;; Author: Aleksandar Dimitrov <https://github.com/adimit>
-;; Maintainer: Aleksandar Dimitrov <mail@aleks.bg>
+;; Author: Aleksandar Dimitrov <code@aleks.bg>
+;; Maintainer: Aleksandar Dimitrov <code@aleks.bg>
 ;; Created: Juli 23, 2021
 ;; Modified: Juli 23, 2021
-;; Version: 0.0.1
-;; Keywords: Jira, REST API
-;; Homepage: https://github.com/aleks/jira
-;; Package-Requires: ((emacs "27.0"))
+;; Version: 0.1.0
+;; Keywords: tools, comm
+;; Homepage: https://github.com/adimit/xjira
+;; Package-Requires: ((emacs "27.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
-;;  Interact with itdesign xjira. This library requires Emacs be built with
+;;  Interact with Jira. This library requires Emacs be built with
 ;;  libjansson support. Use
 ;;
 ;;    (functionp 'json-parse-buffer)
@@ -22,7 +22,9 @@
 ;;  to check if yours is.
 ;;
 ;;; Description:
-;;  Use `xjira-get-issue' to download issue information from the itdesign xjira instance.
+;;  First, set `xjira-host' and `xjira-auth'. If they're unset, you will be prompted
+;;  for their values as soon as you try to use any function.
+;;  Use `xjira-get-issue' to download issue information from a Jira instance.
 ;;
 ;;  The authentication needs to be provided in base64 encoded form. Use something like
 ;;
@@ -34,71 +36,90 @@
 ;;
 ;;  You can also create a temporary file with the above string and base64 encode
 ;;  that. This bypasses the shell history. Just make sure the editor does not
-;;  sneak in a final newline. Use hexdump -C to make sure.
+;;  sneak in a final newline. Use hexdump -C to check.
 ;;
 ;;; Code:
 
-(defvar xjira--org-capture-latest-issue-result nil
-  "Stores the latest result of an org capture process.")
+;;; Private Utility:
 
-(defvar xjira-auth nil
-  "Your base64 encoded username:password string to access the Jira REST API.")
-
-(defun xjira--get-url (path auth-string)
-    "Retrieve PATH using AUTH-STRING from XJIRA."
-  (let ((jira-url (concat "http://xjira/rest/api/2/" path))
-        (url-request-extra-headers `(("Content-Type" . "application/json")
-                                     ("Authorization" . ,(concat "Basic " auth-string)))))
+(defun xjira--get-url (path host auth)
+  "Retrieve PATH using AUTH from HOST."
+  (let* ((jira-url (concat host "/rest/api/2/" path))
+         (jira-auth (concat "Basic " auth))
+         (url-request-extra-headers `(("Content-Type" . "application/json")
+                                      ("Authorization" . ,jira-auth))))
     (with-temp-buffer
       (url-insert-file-contents jira-url)
       (json-parse-buffer :object-type 'alist))))
 
-(defun xjira--get-issue (issue auth)
-  "Fetch ISSUE from Jira using AUTH.
-The result is an alist with issue, issue-type, title, reporter and description assocs."
+(defvar xjira--org-capture-latest-issue-result nil
+  "Stores the latest result of an org capture process.")
 
-  (let* ((jira-result (xjira--get-url (concat "issue/" issue) auth))
-       (issue (alist-get 'key jira-result))
-       (issue-type (alist-get 'name (alist-get 'issuetype (alist-get 'fields jira-result))))
-       (title (alist-get 'summary (alist-get 'fields jira-result)))
-       (reporter (alist-get 'displayName (alist-get 'reporter (alist-get 'fields jira-result))))
-       (description (alist-get 'description (alist-get 'fields jira-result))))
+(defun xjira--get-issue (issue host auth)
+  "Fetch ISSUE from HOST using AUTH.
+The result is an alist with issue, issue-type, title, reporter and description assocs."
+  (let* ((jira-result (xjira--get-url (concat "issue/" issue) host auth))
+         (issue (alist-get 'key jira-result))
+         (issue-type (alist-get 'name (alist-get 'issuetype (alist-get 'fields jira-result))))
+         (title (alist-get 'summary (alist-get 'fields jira-result)))
+         (reporter (alist-get 'displayName (alist-get 'reporter (alist-get 'fields jira-result))))
+         (description (alist-get 'description (alist-get 'fields jira-result))))
     `((issue . ,issue)
       (issue-type . ,issue-type)
       (title . ,title)
       (reporter . ,reporter)
-      (description . ,description))))
+      (description . ,description)
+      (raw . ,jira-result))))
+
+(defun xjira--obj-or-null (obj)
+  "Return OBJ as is unless it is :null, in which case return the empty string."
+  (if (eq :null obj) "" obj))
+
+(defun xjira--get-last (symbol)
+  "Get SYMBOL from the last captured Jira ticket conveniently."
+  (xjira--obj-or-null (alist-get symbol xjira--org-capture-latest-issue-result)))
+
+(defun xjira--strip-cr (string)
+  "Strip carriage-returns (^M) from STRING."
+  (replace-regexp-in-string "\r" "" string))
+
+;;; Public API:
+
+(defvar xjira-host nil
+  "The protocol and host of your Jira instance, without a final slash, e.g. `http://my-jira'.")
+
+(defvar xjira-auth nil
+  "Your base64 encoded username:password string to access the Jira REST API.")
 
 (defun xjira-org-capture-issue (&optional project)
   "Prompt for an issue number and retrieve it from Jira PROJECT.
-Will insert the issue's number in the buffer and store the issue's details for later use."
+Will the issue's details for later use with `xjira-get*' functions.
+Will return nil as to allow for calling it at the start of a capture template
+without inserting any text."
   (interactive)
   (let* ((project (if project project (read-string "Jira project: ")))
          (issue-number (concat project "-" (read-string (concat project "-")))))
-    (unless xjira-auth (setq xjira-auth (read-string "Jira auth: ")))
+    (unless xjira-host (setq xjira-host (read-string "Jira host (e.g. http://my-jira): ")))
+    (unless xjira-auth (setq xjira-auth (read-string "Jira auth (echo -n user:pass | base64 -): ")))
     (setq xjira--org-capture-latest-issue-result
-          (xjira--get-issue issue-number xjira-auth)))
-  (xjira-get-captured-issue))
+          (xjira--get-issue issue-number xjira-host xjira-auth)))
+  nil)
 
 (defun xjira-get-captured-issue ()
   "Get the last captured jira issue."
-  (alist-get 'issue xjira--org-capture-latest-issue-result))
+  (xjira--get-last 'issue))
 
 (defun xjira-get-captured-description ()
   "Get the last captured jira issue description."
-  (replace-regexp-in-string "\r" "" (xjira--obj-or-null (alist-get 'description xjira--org-capture-latest-issue-result))))
+  (xjira--strip-cr (xjira--get-last 'description)))
 
 (defun xjira-get-captured-reporter ()
   "Get the last captured jira issue reporter."
-  (xjira--obj-or-null (alist-get 'reporter xjira--org-capture-latest-issue-result)))
+  (xjira--get-last 'reporter))
 
 (defun xjira-get-captured-title ()
   "Get the last captured jira issue title."
-  (xjira--obj-or-null (alist-get 'title xjira--org-capture-latest-issue-result)))
-
-(defun xjira--obj-or-null (obj)
-  "Return OBJ unless it is :null, in which case return the empty string."
-  (if (eq :null obj) "" obj))
+  (xjira--obj-or-null 'title))
 
 (provide 'xjira)
 ;;; xjira.el ends here
